77c370de710725ea1c4c4d349e46e0dad51234b6
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2021-08-21 13:34:40 +0600

    Update rdp.py

--- rdp.py
+++ rdp.py
@@ -4,6 +4,25 @@
 import os
 import subprocess
 
+#Create User
+username = "hello" #@param {type:"string"}
+password = "root" #@param {type:"string"}
+
+print("Creating User and Setting it up")
+
+# Creation of user
+os.system(f"useradd -m {username}")
+
+# Add user to sudo group
+os.system(f"adduser {username} sudo")
+    
+# Set password of user to 'root'
+os.system(f"echo '{username}:{password}' | sudo chpasswd")
+
+# Change default shell from sh to bash
+os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
+
+print("User Created and Configured")
 #@markdown  Visit http://remotedesktop.google.com/headless and Copy the command after authentication
 
 CRP = "" #@param {type:"string"}
